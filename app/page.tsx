672c9c67d8eb53f06c7f8179775b44546e06fde7
Henrique Ferraz <henriqueferraz@ofnet.com.br>
2024-04-17 10:42:39 -0300

    17/04

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
-export default function Home() {
+"use client"
+
+import { Footer } from "@/components/footer";
+import { Header } from "@/components/header";
+import { TableCrud } from "@/components/table";
+
+const Page = () => {
   return (
-    <div>
-      ...
+    <div className="w-full max-w-4xl mx-auto">
+      <Header />
+      <div className="mx-3">
+        <TableCrud />
+      </div>
+      <Footer />
     </div>
-  );
+  )
 }
+export default Page;
